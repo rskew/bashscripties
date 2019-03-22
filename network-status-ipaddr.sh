285214c5c77c748bb8fbdbfc8d51be257869e4b8
Rowan Skewes <rowan.skewes@gmail.com>
2019-03-22 14:07:41 +1100

    fdsa

--- network-status-ipaddr.sh
+++ network-status-ipaddr.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-iwconfig wlp3s0 2>&1 | grep -q no\ wireless\ extensions\. && {
-    echo wired
-    exit 0
-}
-
 ipaddr=`ifconfig wlp3s0 | awk '/inet / {gsub(" ", "", $2); print $2}'`
 
 echo $ipaddr
--- network-status-ssid.sh
+++ network-status-ssid.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 
-iwconfig wlp3s0 2>&1 | grep -q no\ wireless\ extensions\. && {
-    echo wired
-    exit 0
-}
-
-essid=`iwconfig wlp3s0 | awk -F '"' '/ESSID/ {print $2}'`
+essid=`nmcli | awk '/connected to/ {print $4}'`
 
 echo $essid
 
