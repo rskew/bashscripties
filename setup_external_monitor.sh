30e9af1b86b991e0b90af99b9ad35e7c6651f112
Rowan Skewes <rowan.skewes@gmail.com>
2020-07-03 20:04:06 +1000

    Remove old unused scripts

--- setup_external_monitor.sh
+++ setup_external_monitor.sh
@@ -1,30 +1,38 @@
 #!/usr/bin/env bash
 # Automatically setup external monitor
 
-is_vga_connected=`DISPLAY=:0 xrandr | sed -n '/VGA1 connected/p'`
+POSITION_EXT_TO_LAPPY="left-of"
+#POSITION_EXT_TO_LAPPY="above"
 
-is_dp1_connected=`DISPLAY=:0 xrandr | sed -n '/DP1 connected/p'`
-is_hdmi1_connected=`DISPLAY=:0 xrandr | sed -n '/HDMI1 connected/p'`
+is_vga_connected=`DISPLAY=:0 xrandr | sed -n '/VGA-1 connected/p'`
+
+is_dp1_connected=`DISPLAY=:0 xrandr | sed -n '/DP-1 connected/p'`
+is_hdmi1_connected=`DISPLAY=:0 xrandr | sed -n '/HDMI-1 connected/p'`
 
 is_dvi1_connected=`DISPLAY=:0 xrandr | sed -n '/DVI-I-1-1 connected/p'`
 is_dvi2_connected=`DISPLAY=:0 xrandr | sed -n '/DVI-I-2-2 connected/p'`
 
 if [ -n "$is_vga_connected" ]; then
   xrandr --newmode "1680x1050_60.00"  146.25  1680 1784 1960 2240  1050 1053 1059 1089 -hsync +vsync
-  xrandr --addmode VGA1 1680x1050_60.00
-  DISPLAY=:0 xrandr --output VGA1 --auto --above LVDS1 --primary
+  xrandr --addmode VGA-1 1680x1050_60.00
+  DISPLAY=:0 xrandr --output VGA-1 --auto --"$POSITION_EXT_TO_LAPPY" LVDS-1 --primary
 elif [ -n "$is_dp1_connected" ]; then
-  DISPLAY=:0 xrandr --output DP1 --auto --above LVDS1 --primary
+  # for biiiiig screen at work:
+  #xrandr --newmode "3840x2160_30.00"  338.75  3840 4080 4488 5136  2160 2163 2168 2200 -hsync +vsync
+  #xrandr --addmode DP-1 3840x2160_30.00
+  #DISPLAY=:0 xrandr --output DP-1 --auto --"$POSITION_EXT_TO_LAPPY" LVDS-1 --primary --mode "3840x2160_30.00" --verbose
+  #DISPLAY=:0 xrandr --output DP-1 --auto --"$POSITION_EXT_TO_LAPPY" LVDS-1 --primary --mode "3840x2160"
+  DISPLAY=:0 xrandr --output DP-1 --auto --"$POSITION_EXT_TO_LAPPY" LVDS-1 --primary
 elif [ -n "$is_hdmi1_connected" ]; then
-  #DISPLAY=:0 xrandr --output HDMI1 --auto --above LVDS1 --primary
-  DISPLAY=:0 xrandr --output HDMI1 --mode 1920x1080 --above LVDS1 --primary
+  DISPLAY=:0 xrandr --output HDMI-1 --auto --"$POSITION_EXT_TO_LAPPY" LVDS-1 --primary
+  #DISPLAY=:0 xrandr --output HDMI-1 --auto --"$POSITION_EXT_TO_LAPPY" LVDS-1 --primary --mode "800x600"
 elif [ -n "$is_dvi1_connected" ]; then
-    DISPLAY=:0 xrandr --output DVI-I-1-1 --auto --above LVDS1 --primary --rotate normal
-    DISPLAY=:0 xrandr --output DVI-I-2-2 --auto  --left-of DVI-I-1-1 --rotate left
+  DISPLAY=:0 xrandr --output DVI-I-1-1 --auto --"$POSITION_EXT_TO_LAPPY" LVDS-1 --primary --rotate normal
+  DISPLAY=:0 xrandr --output DVI-I-2-2 --auto  --"$POSITION_EXT_TO_LAPPY" DVI-I-1-1 --rotate left
 else
-  DISPLAY=:0 xrandr --output VGA1 --off
-  DISPLAY=:0 xrandr --output DP1 --off
-  DISPLAY=:0 xrandr --output HDMI1 --off
+  DISPLAY=:0 xrandr --output VGA-1 --off
+  DISPLAY=:0 xrandr --output DP-1 --off
+  DISPLAY=:0 xrandr --output HDMI-1 --off
   DISPLAY=:0 xrandr --output DVI-I-1-1 --off
   DISPLAY=:0 xrandr --output DVI-I-2-2 --off
 fi
--- synth-start.sh
+++
@@ -1,5 +0,0 @@
-#fluidsynth -v -f /home/rowan/documents/code/scripts/instrument-setup-fluidsynth.txt -m alsa_seq -s /usr/share/soundfonts/FluidR3_GM.sf2
-## In the text file was:
-## prog 0 46
-
-qsynth -s -a pulseaudio
